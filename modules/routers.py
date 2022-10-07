e11160c8c008f6965fe375bf045fbf4bc3aff956
Leon <limiroshnichenko@miem.hse.ru>
2022-10-07 22:50:22 +0300

    ✨ rewriting to async + caches

--- modules/routers.py
+++ modules/routers.py
@@ -1,29 +1,55 @@
 from datetime import datetime
+import json
 import re
-from settings import HEADER, TZ, ROOT_PATH
+from settings import HEADER, TZ, ROOT_PATH, TAIGA_URL
 from fastapi import APIRouter, Response
 from icalendar import Calendar, Event
-import requests
+import aioredis
+import httpx
 import pytz
 
 
 router = APIRouter(prefix=ROOT_PATH.rstrip("/"))
 
 
+@router.on_event("startup")
+async def start():
+    global client
+    global red
+    client = httpx.AsyncClient(base_url=TAIGA_URL.rstrip("/"))
+    red = aioredis.from_url("redis://redis")
+
+
+@router.on_event("shutdown")
+async def stop():
+    global client
+    global red
+    await client.aclose()
+    await red.close()
+
+
 @router.get("/calendar/{email}")
-def make_calendar(email: str):
+async def make_calendar(email: str):
     ical = Calendar()
     user_slug = email.split("@")[0]
-    tasks = requests.get(
-        "https://track.miem.hse.ru/api/v1/tasks",
+    if await red.exists("users"):
+        user_id = json.loads(await red.get("users"))[user_slug]
+    else:
+        res = await client.get("/api/v1/users", headers=HEADER)
+        hash = {elem["username"]: elem["id"] for elem in res.json()}
+        await red.set(
+            "users",
+            json.dumps(hash),
+            ex=86400
+        )
+        user_id = hash[user_slug]
+    tasks = await client.get(
+        f"/api/v1/tasks?assigned_to={user_id}",
         headers=HEADER
-    ).json()
+    )
 
-    for task in tasks:
-        if task["assigned_to"] is None:
-            continue
-        if task["assigned_to_extra_info"]["username"] == user_slug \
-                                    and task["due_date"] is not None:
+    for task in tasks.json():
+        if task["due_date"] is not None:
             due_date = re.match(r"(\d\d\d\d)-(\d\d)-(\d\d)", task["due_date"])
             event = Event()
             event.add("dtstart", datetime(
